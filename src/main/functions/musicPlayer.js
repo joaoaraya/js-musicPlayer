1f66ac4b4ca1fc14cf2111850d2fd761bae30309
Araya <joaoaraya7@gmail.com>
2021-10-27 14:51:21 -0300

    novo layout responsivo

--- src/main/functions/musicPlayer.js
+++ src/main/functions/musicPlayer.js
@@ -1,12 +1,13 @@
 // --- Importar banco de dados (temporário) das listas de músicas ---
 // Local = no mesmo que este js está
-import {musicas} from './dbMusicas.js';
+import { musicas } from './dbMusicas.js';
 
 // --- Declarações de valores ---
 const musica = document.querySelector('audio');
-const musicaAutor = document.querySelector('.descricao h1');
-const musicaTitulo = document.querySelector('.descricao h2');
+const musicaCapaCover = document.querySelector('.capaCover');
 const musicaCapa = document.querySelector('#capa');
+const musicaAutor = document.querySelector('#titulo');
+const musicaTitulo = document.querySelector('#autor');
 const barra = document.querySelector('progress');
 const tempoAtual = document.querySelector('.atual');
 const tempoTotal = document.querySelector('.total');
@@ -15,6 +16,7 @@ const btnPausa = document.querySelector('#btnPausa');
 const btnVoltar = document.querySelector('#btnVoltar');
 const btnProximo = document.querySelector('#btnProximo');
 const files = './src/uploads/';
+const gradientBG = 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%)';
 
 let musicaIndex = 0;
 
@@ -23,13 +25,13 @@ const tocarMusica = () => {
     musica.play();
     btnPausa.style.display = 'block';
     btnPlay.style.display = 'none';
-};
+}
 
 const pausarMusica = () => {
     musica.pause();
     btnPlay.style.display = 'block';
     btnPausa.style.display = 'none';
-};
+}
 
 const pularMusica = () => {
     musicaIndex++;
@@ -37,7 +39,7 @@ const pularMusica = () => {
         musicaIndex = 0;
     }
     renderizarMusica(musicaIndex);
-};
+}
 
 const voltarMusica = () => {
     musicaIndex--;
@@ -45,13 +47,13 @@ const voltarMusica = () => {
         musicaIndex = musicas.length - 1;
     }
     renderizarMusica(musicaIndex);
-};
+}
 
 // Mudar a barra e o ponto com a porcentagem do progresso da música
 const atualizarBarra = () => {
-    barra.style.width =  parseInt(Number(musica.currentTime) / Number(musica.duration) * 100) + '%';
+    barra.style.width = parseInt(Number(musica.currentTime) / Number(musica.duration) * 100) + '%';
     tempoAtual.textContent = tempo(parseInt(Number(musica.currentTime)));
-};
+}
 
 // Converter os segundos em minutos com formatação '0:00'
 const tempo = (segundos) => {
@@ -61,7 +63,7 @@ const tempo = (segundos) => {
         seg = '0' + seg
     }
     return min + ':' + seg;
-};
+}
 
 // Enviar para o DOM os dados das músicas
 const renderizarMusica = (i) => {
@@ -70,12 +72,13 @@ const renderizarMusica = (i) => {
         musicaTitulo.textContent = musicas[i].titulo;
         musicaAutor.textContent = musicas[i].autor;
         musicaCapa.src = files + 'img/' + musicas[i].img;
+        musicaCapaCover.style.backgroundImage = `${gradientBG}, url('${files + 'img/' + musicas[i].img}')`;
         tempoTotal.textContent = tempo(parseInt(Number(musica.duration)));
         if (i > 0) {
             tocarMusica();
         }
     });
-};
+}
 
 // --- Função inicial ---
 renderizarMusica(musicaIndex);
